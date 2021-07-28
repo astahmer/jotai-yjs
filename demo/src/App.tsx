aa926ef74c604f6ad5ba7329b725dd8d6cab3309
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-07-29 01:28:38 +0200

    chore(demo): rm test page

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -2,7 +2,6 @@ import { Center, ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "./App.css";
 import { Demo } from "./pages/Demo";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Test } from "./pages/Test";
 import { Minimal } from "./pages/Minimal";
 
 const theme = extendTheme({ config: { initialColorMode: "light" } });
@@ -13,7 +12,6 @@ function App() {
             <BrowserRouter>
                 <Center p={[0, "8"]} pt="0" boxSize="100%">
                     <Switch>
-                        <Route path="/test" children={<Test />} />
                         <Route path="/minimal" children={<Minimal />} />
                         <Route path="/" children={<Demo />} />
                     </Switch>
