d7257729f70f33eb34ff4b119ca5f7179ae8a4f2
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-07-29 01:25:17 +0200

    build: automate external from peerDeps

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,18 @@
 import { defineConfig } from "vite";
 import path from "path";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/",
     root: "./",
+    plugins: [peerDepsExternal()],
     build: {
         sourcemap: true,
         lib: {
             entry: path.resolve(__dirname, "src/index.ts"),
             name: "jotai-yjs",
             fileName: (format) => `jotai-yjs.${format}.js`,
-        },
-        rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: ["jotai", "valtio", "valtio-yjs", "react", "yjs", "y-websocket"],
-            output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                    // vue: "Vue",
-                },
-            },
         },
     },
 });
